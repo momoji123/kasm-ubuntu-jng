65b4674cf7c1f1814ccf448dd25f7d79e3ed737b
momoji123 <rozaan01@gmail.com>
2023-12-24 17:50:47 +0100

    try install git desk througt apt

--- src/ubuntu/install/github-desktop/install_github_desktop.sh
+++ src/ubuntu/install/github-desktop/install_github_desktop.sh
@@ -2,7 +2,8 @@
 set -ex
 
 apt-get update
-apt-get install -y wget gdebi-core
+apt-get install -y wget
 
-wget https://github.com/shiftkey/desktop/releases/download/release-3.1.1-linux1/GitHubDesktop-linux-3.1.1-linux1.deb
-gdebi GitHubDesktop-linux-3.1.1-linux1.deb
+wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+apt update && apt install github-desktop
