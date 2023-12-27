035a2a57e70c6324283675b0666cf14c23670f42
momoji123 <rozaan01@gmail.com>
2023-12-27 16:51:19 +0100

    Add veracrypt to desktop

--- src/ubuntu/install/veracrypt/install_veracrypt.sh
+++ src/ubuntu/install/veracrypt/install_veracrypt.sh
@@ -5,3 +5,5 @@ apt-get update
 sudo add-apt-repository ppa:unit193/encryption -y
 apt-get update
 apt-get install veracrypt -y
+cp /usr/share/applications/veracrypt.desktop $HOME/Desktop/
+chmod +x $HOME/Desktop/veracrypt.desktop
