351133b9d4f8600423c78e28f7e51b16f959afec
momoji123 <rozaan01@gmail.com>
2024-10-06 16:25:48 +0200

    update src

--- src/ubuntu/install/gtk/install_restricted_file_chooser.sh
+++ src/ubuntu/install/gtk/install_restricted_file_chooser.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# Just remove xdg-open for Jammy and Noble
+if [[ "$(lsb_release -cs)" == @(jammy|noble) ]]; then
+  rm -f /usr/bin/xdg-open
+  exit 0
+fi
+
 libgtk_deb=libgtk.deb
 ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/amd64/g')
 
